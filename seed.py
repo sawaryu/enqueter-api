516d8a9cdf217bf800391208e804793a32ae3752
sawaryu <sawaryu@sawaryunoMacBook-Pro.local>
2022-02-26 20:00:21 +0900

    create shells and refacotr seed process

--- seed.py
+++ seed.py
@@ -1,3 +1,4 @@
+import click
 from faker import Faker
 from sqlalchemy import text
 
@@ -9,88 +10,96 @@ from api.model.user import User
 from app import app
 from database import db
 
+"""
+# * How to execute *
+$ export FLASK_APP=seed.py
+$ flask seed_execute --stop
+        or
+$ flask seed_execute --no-stop 
+"""
 
-def main():
-    """take in flask app"""
-    with app.app_context():
-        db.session.begin()
-        try:
-            """reset the database"""
-            app.logger.info("Session begin.")
-            db.session.execute(text("SET FOREIGN_KEY_CHECKS = 0"))
+
+@app.cli.command('seed_execute')
+@click.option('--stop/--no-stop', default=False)
+def seed_execute(stop: bool):
+    try:
+        """reset the database"""
+        execute_type = "Only drop." if stop else "All processes"
+        app.logger.info(f"Session begin. execute type is '{execute_type}'")
+        db.session.execute(text("SET FOREIGN_KEY_CHECKS = 0"))
+        db.session.commit()
+        meta = db.metadata
+        for tbl in reversed(meta.sorted_tables):
+            app.logger.info(f'{tbl} dropped.')
+            db.session.execute(text(f'DROP TABLE IF EXISTS {tbl};'))
             db.session.commit()
-            meta = db.metadata
-            for tbl in reversed(meta.sorted_tables):
-                app.logger.info(f'{tbl} dropped.')
-                db.session.execute(text(f'DROP TABLE IF EXISTS {tbl};'))
-                db.session.commit()
-            db.create_all()
-            app.logger.info("Done drop and create tables. And starting create seed date.")
+        if stop:
+            app.logger.info("Drop All models completely and seed processed are stopped.")
+            return
 
-            """Create users *(range(1, 11) > 1~10)"""
-            faker_gen = Faker()
-            for n in range(1, 101):
-                n = str(n)
-                username = f'sample{n + n}'
-                email = f"sample{n}@sample.com"
-                password = 'passpass'
-                introduce = faker_gen.company()
-                role = UserRole.admin if n == "1" else UserRole.user
+        db.create_all()
+        app.logger.info("Done drop and create tables completely. And starting create seed date.")
 
-                user = User(
-                    username=username,
-                    email=email,
-                    password=password,
+        """Create users *(range(1, 11) > 1~10)"""
+        faker_gen = Faker()
+        for n in range(1, 101):
+            n = str(n)
+            username = f'sample{n + n}'
+            email = f"sample{n}@sample.com"
+            password = 'passpass'
+            introduce = faker_gen.company()
+            role = UserRole.admin if n == "1" else UserRole.user
+
+            user = User(
+                username=username,
+                email=email,
+                password=password,
+            )
+            user.introduce = introduce
+            user.role = role
+            db.session.add(user)
+            db.session.flush()
+            confirmation = Confirmation(user.id)
+            confirmation.confirmed = True
+            db.session.add(confirmation)
+            db.session.flush()
+
+            # inset point
+            insert_point = point.insert().values(
+                user_id=user.id,
+                point=3
+            )
+            db.session.execute(insert_point)
+
+        """Create user relationships"""
+        first_user = User.query.filter_by(id=1).first()
+        target_users = User.query.all()
+        for target_user in target_users:
+            first_user.follow(target_user)
+            target_user.follow(first_user)
+            db.session.flush()
+
+        """Create questions"""
+        for n in range(1, 7):
+            for n_2 in range(1, 15):
+                content = faker_gen.address() + "?"
+                option_first = "option1"
+                option_second = "option2"
+                question = Question(
+                    content=content,
+                    user_id=n,
+                    option_first=option_first,
+                    option_second=option_second
                 )
-                user.introduce = introduce
-                user.role = role
-                db.session.add(user)
+                db.session.add(question)
                 db.session.flush()
-                confirmation = Confirmation(user.id)
-                confirmation.confirmed = True
-                db.session.add(confirmation)
-                db.session.flush()
-
-                # inset point
-                insert_point = point.insert().values(
-                    user_id=user.id,
-                    point=3
-                )
-                db.session.execute(insert_point)
-
-            """Create user relationships"""
-            first_user = User.query.filter_by(id=1).first()
-            target_users = User.query.all()
-            for target_user in target_users:
-                first_user.follow(target_user)
-                target_user.follow(first_user)
-                db.session.flush()
-
-            """Create questions"""
-            for n in range(1, 7):
-                for n_2 in range(1, 15):
-                    content = faker_gen.address() + "?"
-                    option_first = "option1"
-                    option_second = "option2"
-                    question = Question(
-                        content=content,
-                        user_id=n,
-                        option_first=option_first,
-                        option_second=option_second
-                    )
-                    db.session.add(question)
-                    db.session.flush()
-            db.session.execute(text("SET FOREIGN_KEY_CHECKS = 1"))
-            db.session.commit()
-            app.logger.info("Successfully created data.")
-        except:
-            app.logger.error("Something fatal error occurred and start rollback.")
-            db.session.rollback()
-            raise
-        finally:
-            app.logger.info("Session had closed.")
-            db.session.close()
-
-
-if __name__ == '__main__':
-    main()
+        db.session.execute(text("SET FOREIGN_KEY_CHECKS = 1"))
+        db.session.commit()
+        app.logger.info("Successfully created data.")
+    except:
+        app.logger.error("Something fatal error occurred and start rollback.")
+        db.session.rollback()
+        raise
+    finally:
+        app.logger.info("Session had closed.")
+        db.session.close()
---
+++ shell/seed_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export FLASK_APP=seed.py
+flask seed_execute --stop
