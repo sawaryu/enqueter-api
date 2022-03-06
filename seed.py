96b020304bf6393bc7e18a67b966682a277f9583
sawaryu <sawaryu@sawaryunoMacBook-Pro.local>
2022-03-06 23:09:31 +0900

    create samples

--- seed.py
+++ seed.py
@@ -44,7 +44,7 @@ def seed_execute(stop: bool):
 
         """Create users *ex: (range(1, 11) > 1~10)"""
         faker_gen = Faker()
-        for n in range(1, 51):
+        for n in range(1, 31):
             n = str(n)
             username = f'sample{n + n}'
             email = f"sample{n}@sample.com"
@@ -66,7 +66,7 @@ def seed_execute(stop: bool):
             db.session.add(confirmation)
             db.session.flush()
 
-            # inset point
+            # insert point
             insert_point = point.insert().values(
                 user_id=user.id,
                 point=3
@@ -94,27 +94,26 @@ def seed_execute(stop: bool):
         db.session.flush()
 
         """Create user relationships"""
-        first_user = User.query.filter_by(id=1).first()
+        first_user = User.find_by_id(1)
+        test_user = User.find_by_email("test@test.com")
         target_users = User.query.all()
         for target_user in target_users:
             first_user.follow(target_user)
+            test_user.follow(target_user)
             target_user.follow(first_user)
+            target_user.follow(test_user)
             db.session.flush()
 
         """Create questions"""
-        for n in range(1, 7):
-            for n_2 in range(1, 15):
-                content = faker_gen.address() + "?"
-                option_first = "option1"
-                option_second = "option2"
-                question = Question(
-                    content=content,
-                    user_id=n,
-                    option_first=option_first,
-                    option_second=option_second
-                )
-                db.session.add(question)
-                db.session.flush()
+        for (index, q) in enumerate(question_samples):
+            question = Question(
+                content=q["content"],
+                user_id=index + 1,
+                option_first=q["option_first"],
+                option_second=q["option_second"]
+            )
+            db.session.add(question)
+            db.session.flush()
         app.logger.info("Successfully created data.")
     except:
         app.logger.error("Something fatal error occurred and start rollback.")
@@ -125,3 +124,27 @@ def seed_execute(stop: bool):
         db.session.commit()
         db.session.close()
         app.logger.info("Session had closed.")
+
+
+question_samples: list[object] = [
+    {"content": "たけのこの里orきのこの山", "option_first": "たけのこの里", "option_second": "きのこの山"},
+    {"content": "好きな女優はどっち？", "option_first": "浜辺美波", "option_second": "広瀬すず"},
+    {"content": "iPhone? Android?", "option_first": "iPhone", "option_second": "Android"},
+    {"content": "Ruby or Python", "option_first": "Ruby", "option_second": "Python"},
+    {"content": "Mac or Windows", "option_first": "Mac", "option_second": "Windows"},
+    {"content": "分譲と賃貸どっちがいい？", "option_first": "分譲", "option_second": "賃貸"},
+    {"content": "生まれ変わるなら？", "option_first": "女性", "option_second": "男性"},
+    {"content": "洋画をみるとき", "option_first": "字幕", "option_second": "吹替"},
+    {"content": "眠いときはどっちを飲む？", "option_first": "レッドブル", "option_second": "モンスターエナジー"},
+    {"content": "値段は関係なくうまいのは？", "option_first": "マック", "option_second": "モスバーガー"},
+    {"content": "Nike vs Adidas", "option_first": "Nike", "option_second": "Adidas"},
+    {"content": "寝る前スマホをみる？", "option_first": "yes", "option_second": "no"},
+    {"content": "夏といえば？", "option_first": "TUBE", "option_second": "サザンオールスターズ"},
+    {"content": "Twitter vs Instagram", "option_first": "Twitter", "option_second": "Instagram"},
+    {"content": "B'zの代表曲", "option_first": "UltraSoul", "option_second": "Ocean"},
+    {"content": "How are you?", "option_first": "Good!", "option_second": "I’m sick"},
+    {"content": "野球？サッカー？", "option_first": "野球", "option_second": "サッカー"},
+    {"content": "ペプシとコーラ美味しいのは？", "option_first": "ペプシ", "option_second": "コーラ"},
+    {"content": "どっち派？", "option_first": "明日花キララ", "option_second": "三上悠亜"},
+    {"content": "どっち派？", "option_first": "欅坂", "option_second": "乃木坂"},
+]
