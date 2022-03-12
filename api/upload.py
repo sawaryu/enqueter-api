28da4caa072216611f3f00c236a0ad007eb4349b
sawaryu <sawaryu@sawaryunoMacBook-Pro.local>
2022-03-12 16:21:07 +0900

    fix boto

--- api/upload.py
+++ api/upload.py
@@ -19,9 +19,9 @@ upload_ns = Namespace('/upload', description="masked(can`t open)")
 
 client = boto3.client(
     "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
-    region_name=os.getenv("AWS_REGION"),
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", "sample"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", "sample"),
+    region_name=os.getenv("AWS_REGION", "sample"),
 )
 
 
